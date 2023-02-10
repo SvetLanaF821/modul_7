e0c73a9f6dacad175bc2dc99c11dc04de6f45b10
SvetLanaF821 <124917580+SvetLanaF821@users.noreply.github.com>
2023-02-10 11:21:33 +0300

    Delete Bad_length.h

--- Bad_length.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-// Исключения для неправильной длины массива
-class Bad_length : public std::exception
-{
-public:
-	virtual const char* what() const noexcept override;	
-};
