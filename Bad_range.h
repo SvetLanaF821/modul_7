21812aca27547913f987166d6434f8bf5d3d8bbe
SvetLanaF821 <124917580+SvetLanaF821@users.noreply.github.com>
2023-02-10 11:22:03 +0300

    Delete Bad_range.h

--- Bad_range.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-// Исключения для выхода за пределы массива
-class Bad_range : public std::exception
-{
-public:
-	virtual const char* what() const noexcept override;	
-};
